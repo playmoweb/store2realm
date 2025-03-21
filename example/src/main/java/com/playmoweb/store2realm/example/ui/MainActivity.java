e64d53dfde63d3a0fc8ef7efefde18df7a2e7bba
Robin Lebranchu <robin@playmoweb.com>
2025-03-21 12:03:41 +0100

    Fix example project build errors

--- example/src/main/java/com/playmoweb/store2realm/example/ui/MainActivity.java
+++ example/src/main/java/com/playmoweb/store2realm/example/ui/MainActivity.java
@@ -1,7 +1,7 @@
 package com.playmoweb.store2realm.example.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
